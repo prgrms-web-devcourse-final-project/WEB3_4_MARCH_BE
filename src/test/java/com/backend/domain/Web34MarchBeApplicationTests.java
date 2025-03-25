606339614dfba5f763e8e4c3dc73f5b909869c09
Gonhub9 <ho_gok@naver.com>
2025-03-25 18:52:16 +0900

    Rename: backend로 이름 수정
     Conflicts:
    	backend/src/main/java/com/backend/BackendApplication.java
    	backend/src/main/java/com/backend/global/base/BaseEntity.java
    	backend/src/main/java/com/backend/global/config/JpaConfig.java
    	backend/src/main/java/com/backend/global/exception/GlobalException.java
    	backend/src/main/java/com/backend/global/response/GenericResponse.java
    	backend/src/test/java/com/backend/domain/BackendApplicationTests.java
    	src/main/java/com/backend/Web34MarchBeApplication.java
    	src/main/java/com/backend/global/baseentity/BaseEntity.java
    	src/main/java/com/backend/global/config/JpaConfig.java
    	src/main/java/com/backend/global/exception/GlobalErrorCode.java
    	src/main/java/com/backend/global/exception/GlobalException.java
    	src/main/java/com/backend/global/response/GenericResponse.java
    	src/main/java/com/team6/Web34MarchBeApplication.java
    	src/main/java/com/team6/global/baseentity/BaseEntity.java
    	src/main/java/com/team6/global/config/JpaConfig.java
    	src/main/java/com/team6/global/exception/GlobalException.java
    	src/main/java/com/team6/global/response/GenericResponse.java
    	src/test/java/com/backend/domain/Web34MarchBeApplicationTests.java
    	src/test/java/com/team6/domain/Web34MarchBeApplicationTests.java

---
+++ src/test/java/com/backend/domain/Web34MarchBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
