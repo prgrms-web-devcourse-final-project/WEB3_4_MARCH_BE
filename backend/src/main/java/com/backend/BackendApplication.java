87909fd1a1579fe4fa8922f68513b93357185ad2
wlsdnjs1120 <wlsdnjs1120@naver.com>
2025-03-26 14:13:28 +0900

    refactor: 프로젝트 초기 디렉토리 구조 세팅 수정

--- backend/src/main/java/com/backend/BackendApplication.java
+++ backend/src/main/java/com/backend/BackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Web34MarchBeApplication {
+public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Web34MarchBeApplication.class, args);
+		SpringApplication.run(BackendApplication.class, args);
 	}
 
 }
