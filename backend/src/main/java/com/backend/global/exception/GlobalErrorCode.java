f44e76810cd791983c8a641ccf6d83da6399105a
Gonhub9 <ho_gok@naver.com>
2025-03-26 14:56:42 +0900

    Feat: 글로벌 에러 코드 작성

--- backend/src/main/java/com/backend/global/exception/GlobalErrorCode.java
+++ backend/src/main/java/com/backend/global/exception/GlobalErrorCode.java
@@ -12,8 +12,10 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum GlobalErrorCode {
 
-    INVALID_REQUEST(HttpStatus.BAD_REQUEST, 400, "잘못된 요청입니다.");
+    INVALID_REQUEST(HttpStatus.BAD_REQUEST, 400, "잘못된 요청입니다."),
 
+    CHAT_NOT_FOUND_BY_ID(HttpStatus.NOT_FOUND, 1001, "사용자를 찾을 수 없습니다."),
+    DUPLICATE_CHAT_REQUEST(HttpStatus.CONFLICT, 1002, "이미 채팅 요청이 존재합니다.");
     private final HttpStatus httpStatus;
     private final int code;
     private final String message;
