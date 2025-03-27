2ec3ca017f34a5dc7d60dc3650f166eefc56fe65
joonaeng <jylee33286315@gmail.com>
2025-03-27 22:36:34 +0900

    feat: 이미지 S3업로드 시 여러장 동시에 List형식으로 받아서 개별적 업로드 가능

--- backend/src/main/java/com/backend/domain/image/controller/PresignedController.java
+++ backend/src/main/java/com/backend/domain/image/controller/PresignedController.java
@@ -1,8 +1,9 @@
 package com.backend.domain.image.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,35 +22,36 @@ import lombok.RequiredArgsConstructor;
 public class PresignedController {
 	private final PresignedService presignedService;
 
-	// @PostMapping("/presign")
-	// public PresignedUrlResponse createPresignedUrl(@PathVariable Long userId,
-	// 	@RequestBody PresignedUrlRequest request) {
-	//
-	// }
+	@PostMapping("/uploads")
+	public ResponseEntity<String> uploadImages(@RequestParam("files") List<MultipartFile> files) throws IOException {
+		List<String> uploadResults  = new ArrayList<>();
 
-	// @PostMapping("/presign")
-	// public PresignedUrlResponse createPresignedUrl(@RequestBody PresignedUrlRequest request) {
-	// 	return presignedService.createPresignedUrl();
-	// }
+		for (MultipartFile file : files) {
+			try {
+				// 1. 파일의 확장자 추출
+				String extension = file.getOriginalFilename().split("\\.")[1];
 
-	@PostMapping("/upload")
-	public ResponseEntity<String> uploadImage(@RequestParam("file") MultipartFile file) throws IOException {
-		// 1. 파일 확장자 추출 (ex: png, jpeg)
-		String extension = file.getOriginalFilename().split("\\.")[1];
+				// 2. 프리사인드 URL 생성
+				String presignedUrl = presignedService.createPresignedUrl(file.getOriginalFilename(), file.getContentType());
 
-		// 2. 프리사인드 URL 생성
-		String presignedUrl = presignedService.createPresignedUrl(file.getOriginalFilename(), file.getContentType());
+				// 3. 파일 바이트로 변환
+				byte[] fileData = file.getBytes();
 
-		// 3. 파일 바이트로 변환
-		byte[] fileData = file.getBytes();
+				// 4. S3에 파일 업로드
+				boolean uploadSuccess = presignedService.uploadFileToS3(presignedUrl, fileData);
 
-		// 4. S3에 파일 업로드
-		boolean uploadSuccess = presignedService.uploadFileToS3(presignedUrl, fileData);
-
-		if (uploadSuccess) {
-			return ResponseEntity.ok("파일 업로드 성공");
-		} else {
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("파일 업로드 실패");
+				if (uploadSuccess) {
+					uploadResults.add(file.getOriginalFilename() + " 업로드 성공");
+				} else {
+					uploadResults.add(file.getOriginalFilename() + " 업로드 실패");
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				// 에러가 발생한 경우 해당 파일명을 결과에 추가
+				uploadResults.add(file.getOriginalFilename() + " 업로드 중 오류 발생");
+				e.printStackTrace();
+			}
 		}
+		return ResponseEntity.ok("업로드 결과: " + String.join(", ", uploadResults));
 	}
 }
