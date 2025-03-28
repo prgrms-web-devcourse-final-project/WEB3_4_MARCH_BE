6149f0e80702a68e69eba9695f09fbb8cf88640d
Gonhub9 <ho_gok@naver.com>
2025-03-28 22:50:30 +0900

    Test: api 추가

--- src/main/resources/static/chat-test.js
+++ src/main/resources/static/chat-test.js
@@ -6,7 +6,7 @@ stompClient.connect({}, function () {
     console.log("WebSocket 연결 성공!");
 
     // 특정 채팅방 구독
-    stompClient.subscribe("/sub/chat/123", function (message) {
+    stompClient.subscribe("/api/sub/chat/123", function (message) {
       console.log("받은 메시지:", JSON.parse(message.body));
     });
 });
@@ -14,6 +14,6 @@ stompClient.connect({}, function () {
 // 메시지 보내기
 function sendMessage() {
     const message = { roomId: "123", sender: "user1", content: "안녕하세요!" };
-    stompClient.send("/pub/chat/send", {}, JSON.stringify(message));
+    stompClient.send("/api/pub/chat/send", {}, JSON.stringify(message));
 }
 
