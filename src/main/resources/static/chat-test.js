287ebc0a5cdbb5422ff6b4007a13d024b45dca9e
Gonhub9 <ho_gok@naver.com>
2025-03-28 17:03:59 +0900

    Test: WebSocket, Kafka, Redis 테스트를 위한 HTML, JS

---
+++ src/main/resources/static/chat-test.js
@@ -0,0 +1,19 @@
+// SockJS 연결할 때 포트 포함한 전체 주소로!
+const socket = new SockJS("/ws");
+const stompClient = Stomp.over(socket);
+
+stompClient.connect({}, function () {
+    console.log("WebSocket 연결 성공!");
+
+    // 특정 채팅방 구독
+    stompClient.subscribe("/sub/chat/123", function (message) {
+      console.log("받은 메시지:", JSON.parse(message.body));
+    });
+});
+
+// 메시지 보내기
+function sendMessage() {
+    const message = { roomId: "123", sender: "user1", content: "안녕하세요!" };
+    stompClient.send("/pub/chat/send", {}, JSON.stringify(message));
+}
+
