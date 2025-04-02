bdba9be77481bfa34ffca778fa1859f15063d58d
richard3251 <paladog31@naver.com>
2025-04-02 19:58:06 +0900

    feat: 회원차단기능 구현
    # Conflicts:
    #	backend/src/main/resources/application.yml
    
    # Conflicts:
    #	backend/src/main/java/com/backend/global/exception/GlobalErrorCode.java
    #	backend/src/main/resources/application.yml
    
    # Conflicts:
    #	.gitignore
    #	backend/src/main/java/com/backend/domain/blockuser/controller/BlockUserController.java
    #	backend/src/main/java/com/backend/domain/blockuser/dto/request/BlockUserRequest.java
    #	backend/src/main/java/com/backend/domain/blockuser/entity/BlockUser.java
    #	backend/src/main/java/com/backend/domain/blockuser/repository/BlockUserRepository.java
    #	backend/src/main/java/com/backend/domain/blockuser/service/BlockUserService.java
    #	backend/src/main/java/com/backend/global/exception/GlobalErrorCode.java

--- backend/src/main/java/com/backend/domain/blockuser/repository/BlockUserRepository.java
+++ backend/src/main/java/com/backend/domain/blockuser/repository/BlockUserRepository.java
@@ -1,11 +1,7 @@
 package com.backend.domain.blockuser.repository;
 
 import com.backend.domain.blockuser.entity.BlockUser;
-import com.backend.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-import java.util.Optional;
 
 public interface BlockUserRepository extends JpaRepository<BlockUser, Long> {
 
