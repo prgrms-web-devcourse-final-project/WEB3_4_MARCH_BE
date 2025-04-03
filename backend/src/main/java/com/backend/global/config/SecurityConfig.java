56bba8b734f44b7e1cba378b32a1bbb984a86af1
joonaeng <jylee33286315@gmail.com>
2025-04-03 16:40:17 +0900

    test: 인증 테스트 중 회원가입 api 필터체인 추가

--- backend/src/main/java/com/backend/global/config/SecurityConfig.java
+++ backend/src/main/java/com/backend/global/config/SecurityConfig.java
@@ -54,6 +54,7 @@ public class SecurityConfig {
                                 "/v3/api-docs/**",
                                 "/swagger-resources/**",
                                 "/webjars/**",
+                                "/api/members/register",
                                 "/api/members/*/notifications/test"
                         ).permitAll().anyRequest().authenticated()
                 )
