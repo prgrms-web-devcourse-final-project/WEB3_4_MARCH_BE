fcf5275d50dcb737c25ef16c3e06b306f1340940
joonaeng <jylee33286315@gmail.com>
2025-04-04 16:05:44 +0900

    feat: 좋아하는 사람 목록 프로필 조회로 변경, Hibernate프록시 반환 오류 수정

--- backend/src/main/java/com/backend/domain/like/dto/LikeMemberProfileDto.java
+++ backend/src/main/java/com/backend/domain/like/dto/LikeMemberProfileDto.java
@@ -1,14 +1,14 @@
 package com.backend.domain.like.dto;
 
-import com.backend.domain.image.entity.Image;
+import com.backend.domain.image.dto.ProfileImageDto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public class MemberProfileDto {
+public class LikeMemberProfileDto {
     private Long id;
     private String nickname;
-    private Image profileImage;
+    private ProfileImageDto profileImage;
 }
