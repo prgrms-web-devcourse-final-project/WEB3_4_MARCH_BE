f03821c83c0a4e7374116fb5dbcb22b945ab6ded
wlsdnjs1120 <wlsdnjs1120@naver.com>
2025-04-04 17:35:00 +0900

    refactor: JwtUtil 과 TokenProvider의 역할을 명확히 구분하는 리팩토링 수행

--- backend/src/main/java/com/backend/global/auth/kakao/util/JwtUtil.java
+++ backend/src/main/java/com/backend/global/auth/kakao/util/JwtUtil.java
@@ -1,30 +1,20 @@
 package com.backend.global.auth.kakao.util;
 
 import com.backend.global.auth.model.CustomUserDetails;
-import com.backend.global.exception.GlobalErrorCode;
-import com.backend.global.exception.GlobalException;
-import io.jsonwebtoken.*;
-import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.security.Keys;
-import jakarta.annotation.PostConstruct;
+import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 
-import java.security.Key;
-import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 
 /**
  * JWT를 통한 인증 정보를 처리하는 유틸리티 클래스
- * - 토큰 파싱
- * - 사용자 인증 정보 추출
- * - JWT 관련 예외 처리 담당
+ * JwtUtil은 JWT를 기반으로 사용자 인증(Authentication) 객체를 생성 (Spring Security와 연결)
+ * JWT 파싱, 검증 등의 유틸리티는 TokenProvider가 담당한다.
  */
 
 @Slf4j
@@ -32,79 +22,10 @@ import java.util.List;
 @RequiredArgsConstructor
 public class JwtUtil {
 
-    @Value("${spring.security.jwt.secret}")
-    private String secretKey;
-
-    private Key key;
-
-    @Value("${spring.security.jwt.access-token.expiration}")
-    private long ACCESS_TOKEN_EXPIRATION_TIME; // 6시간 (단위: ms)
-
-    @Value("${spring.security.jwt.refresh-token.expiration}")
-    private long REFRESH_TOKEN_EXPIRATION_TIME; // 60일(약 2달) (단위: ms)
-
-    public Long getAccessTokenExpirationTime() {
-        return ACCESS_TOKEN_EXPIRATION_TIME;
-    }
-
-    public Long getRefreshTokenExpirationTime() {
-        return REFRESH_TOKEN_EXPIRATION_TIME;
-    }
-
-
-    // secretKey를 기반으로 Key key를 초기화
-    @PostConstruct
-    public void init() {
-//        this.key = Keys.hmacShaKeyFor(secretKey.getBytes());
-        this.key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
-        log.debug("🔑 key hash: {}", Arrays.hashCode(key.getEncoded()));
-
-    }
-
-
-    public String createAccessToken(Long memberId, String email) {
-        return Jwts.builder()
-                .setSubject(email)
-                .claim("id", memberId)
-                .claim("email", email)
-                .claim("role", "ROLE_USER") //사용자의 권한(권리/역할)을 설정 (ROLE_USER면 일반 사용자)
-                .setExpiration(new Date(System.currentTimeMillis() + ACCESS_TOKEN_EXPIRATION_TIME))
-                .signWith(key)
-                .compact();
-    }
-
-    public String createRefreshToken(Long memberId) {
-        return Jwts.builder()
-                .setSubject("RefreshToken")
-                .claim("id", memberId)
-                .setExpiration(new Date(System.currentTimeMillis() + REFRESH_TOKEN_EXPIRATION_TIME))
-                .signWith(key)
-                .compact();
-    }
-
-    public boolean validateToken(String token) {
-        try {
-            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
-            return true;
-        } catch (ExpiredJwtException e) {
-            log.error("JWT 검증 실패: {}", e.getMessage());
-            throw new GlobalException(GlobalErrorCode.TOKEN_EXPIRED);
-        } catch (MalformedJwtException | SecurityException | UnsupportedJwtException | IllegalArgumentException e) {
-            log.error("JWT 검증 실패: {}", e.getMessage());
-            throw new GlobalException(GlobalErrorCode.INVALID_TOKEN);
-        }
-    }
-
-    public Claims parseToken(String token) {
-        return Jwts.parserBuilder()
-                .setSigningKey(key)
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
-    }
+    private final TokenProvider tokenProvider;
 
     public Authentication getAuthentication(String token) {
-        Claims claims = parseToken(token);
+        Claims claims = tokenProvider.parseToken(token);
 
         Long memberId = claims.get("id", Long.class);
         String email = claims.get("email", String.class);
@@ -118,4 +39,77 @@ public class JwtUtil {
 
         return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
     }
+
+//    @Value("${spring.security.jwt.secret}")
+//    private String secretKey;
+//
+//    private Key key;
+//
+//    @Value("${spring.security.jwt.access-token.expiration}")
+//    private long ACCESS_TOKEN_EXPIRATION_TIME; // 6시간 (단위: ms)
+//
+//    @Value("${spring.security.jwt.refresh-token.expiration}")
+//    private long REFRESH_TOKEN_EXPIRATION_TIME; // 60일(약 2달) (단위: ms)
+//
+//    public Long getAccessTokenExpirationTime() {
+//        return ACCESS_TOKEN_EXPIRATION_TIME;
+//    }
+//
+//    public Long getRefreshTokenExpirationTime() {
+//        return REFRESH_TOKEN_EXPIRATION_TIME;
+//    }
+//
+//
+//    // secretKey를 기반으로 Key key를 초기화
+//    @PostConstruct
+//    public void init() {
+////        this.key = Keys.hmacShaKeyFor(secretKey.getBytes());
+//        this.key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
+//        log.debug("🔑 key hash: {}", Arrays.hashCode(key.getEncoded()));
+//
+//    }
+//
+//
+//    public String createAccessToken(Long memberId, String email) {
+//        return Jwts.builder()
+//                .setSubject(email)
+//                .claim("id", memberId)
+//                .claim("email", email)
+//                .claim("role", "ROLE_USER") //사용자의 권한(권리/역할)을 설정 (ROLE_USER면 일반 사용자)
+//                .setExpiration(new Date(System.currentTimeMillis() + ACCESS_TOKEN_EXPIRATION_TIME))
+//                .signWith(key)
+//                .compact();
+//    }
+//
+//    public String createRefreshToken(Long memberId) {
+//        return Jwts.builder()
+//                .setSubject("RefreshToken")
+//                .claim("id", memberId)
+//                .setExpiration(new Date(System.currentTimeMillis() + REFRESH_TOKEN_EXPIRATION_TIME))
+//                .signWith(key)
+//                .compact();
+//    }
+//
+//    public boolean validateToken(String token) {
+//        try {
+//            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+//            return true;
+//        } catch (ExpiredJwtException e) {
+//            log.error("JWT 검증 실패: {}", e.getMessage());
+//            throw new GlobalException(GlobalErrorCode.TOKEN_EXPIRED);
+//        } catch (MalformedJwtException | SecurityException | UnsupportedJwtException | IllegalArgumentException e) {
+//            log.error("JWT 검증 실패: {}", e.getMessage());
+//            throw new GlobalException(GlobalErrorCode.INVALID_TOKEN);
+//        }
+//    }
+//
+//    public Claims parseToken(String token) {
+//        return Jwts.parserBuilder()
+//                .setSigningKey(key)
+//                .build()
+//                .parseClaimsJws(token)
+//                .getBody();
+//    }
+
+
 }
