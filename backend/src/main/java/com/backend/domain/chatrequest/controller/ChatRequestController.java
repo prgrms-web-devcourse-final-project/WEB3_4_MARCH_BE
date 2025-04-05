9f83bf920117cc9412ee30ef28b15f141f19dc33
richard3251 <paladog31@naver.com>
2025-04-05 19:33:49 +0900

    feat: 채팅요청에 알림기능 부착완ë료.

--- backend/src/main/java/com/backend/domain/chatrequest/controller/ChatRequestController.java
+++ backend/src/main/java/com/backend/domain/chatrequest/controller/ChatRequestController.java
@@ -1,78 +1,85 @@
 package com.backend.domain.chatrequest.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import com.backend.domain.chatrequest.dto.response.ChatRequestDto;
+import com.backend.domain.chatrequest.service.ChatRequestService;
+import com.backend.global.auth.model.CustomUserDetails;
+import com.backend.global.response.GenericResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/chat-request")
 public class ChatRequestController {
 
-   // private final ChatRequestService chatRequestService;
-   //
-   // /**
-   //  * 채팅요청을 보낼시 호출될 api
-   //  *
-   //  * @param loginUser 로그인된 사용자 정보 (요청자)
-   //  * @param receiverId 채팅 요청을 받을 사용자 id
-   //  * @return 처리 결과 메시지
-   //  */
-   // @PostMapping("/{receiverId}")
-   // public GenericResponse<Void> sendRequest(
-   //         @AuthenticationPrincipal CustomUserDetails loginUser,
-   //         @PathVariable("receiverId") Long receiverId
-   //         ) {
-   //     chatRequestService.sendChatRequest(loginUser, receiverId);
-   //
-   //     return GenericResponse.ok("채팅요청을 보냈습니다.");
-   // }
-   //
-   // /**
-   //  * 클라이언트에서 수락/거절 을 누를시 호출될 api
-   //  *
-   //  * @param requestId 처리할 ChatRequest ID
-   //  * @param accept true면 수락, false면 거절
-   //  * @return 처리 결과 메시지
-   //  */
-   // @PostMapping("/respond/{requestId}")
-   // public GenericResponse<Void> respond(
-   //         @PathVariable("requestId") Long requestId,
-   //         @RequestParam boolean accept
-   // ) {
-   //     chatRequestService.respondToRequest(requestId, accept);
-   //
-   //     return GenericResponse.ok("요청 처리 완료.");
-   // }
-   //
-   // /**
-   //  *
-   //  * @param loginUser 로그인된 사용자 정보
-   //  * @return
-   //  */
-   // @GetMapping("/sent-list")
-   // public GenericResponse<List<ChatRequestDto>> getSentRequests(
-   //         @AuthenticationPrincipal CustomUserDetails loginUser
-   // ) {
-   //     List<ChatRequestDto> list =  chatRequestService.getSentRequests(loginUser);
-   //
-   //     return GenericResponse.ok(list, "보낸 요청 목록 조회 성공");
-   // }
-   //
-   // /**
-   //  *
-   //  * @param loginUser 로그인된 사용자 정보
-   //  * @return
-   //  */
-   // @GetMapping("/received-list")
-   // public GenericResponse<List<ChatRequestDto>> getReceivedRequests(
-   //         @AuthenticationPrincipal CustomUserDetails loginUser
-   // ) {
-   //     List<ChatRequestDto> list = chatRequestService.getReceivedRequests(loginUser);
-   //
-   //     return GenericResponse.of(list, "받은 요청 목록 조회 성공");
-   // }
+    private final ChatRequestService chatRequestService;
+
+    /**
+     * 채팅요청을 보낼시 호출될 api
+     *
+     * @param loginUser 로그인된 사용자 정보 (요청자)
+     * @param receiverId 채팅 요청을 받을 사용자 id
+     * @return 처리 결과 메시지
+     */
+    @PostMapping("/{receiverId}")
+    public GenericResponse<Void> sendRequest(
+            @AuthenticationPrincipal CustomUserDetails loginUser,
+            @PathVariable("receiverId") Long receiverId
+            ) {
+        chatRequestService.sendChatRequest(loginUser, receiverId);
+
+        return GenericResponse.ok("채팅요청을 보냈습니다.");
+    }
+
+    /**
+     * 클라이언트에서 수락/거절 을 누를시 호출될 api
+     *
+     * @param requestId 처리할 ChatRequest ID
+     * @param accept true면 수락, false면 거절
+     * @return 처리 결과 메시지
+     */
+    @PostMapping("/respond/{requestId}")
+    public GenericResponse<Void> respond(
+            @PathVariable("requestId") Long requestId,
+            @RequestParam boolean accept
+    ) {
+        chatRequestService.respondToRequest(requestId, accept);
+
+        return GenericResponse.ok("요청 처리 완료.");
+    }
+
+    /**
+     * 내가 보낸 채팅요청 목록 조회 api
+     *
+     * @param loginUser 로그인된 사용자 정보
+     * @return
+     */
+    @GetMapping("/sent-list")
+    public GenericResponse<List<ChatRequestDto>> getSentRequests(
+            @AuthenticationPrincipal CustomUserDetails loginUser
+    ) {
+        List<ChatRequestDto> list =  chatRequestService.getSentRequests(loginUser);
+
+        return GenericResponse.ok(list, "보낸 요청 목록 조회 성공");
+    }
+
+    /**
+     * 내가 받은 채팅요청 목록 조회 api
+     *
+     * @param loginUser 로그인된 사용자 정보
+     * @return
+     */
+    @GetMapping("/received-list")
+    public GenericResponse<List<ChatRequestDto>> getReceivedRequests(
+            @AuthenticationPrincipal CustomUserDetails loginUser
+    ) {
+        List<ChatRequestDto> list = chatRequestService.getReceivedRequests(loginUser);
+
+        return GenericResponse.of(list, "받은 요청 목록 조회 성공");
+    }
 
 
 }
--- backend/src/main/java/com/backend/domain/chatrequest/service/ChatRequestService.java
+++ backend/src/main/java/com/backend/domain/chatrequest/service/ChatRequestService.java
@@ -1,86 +1,115 @@
 package com.backend.domain.chatrequest.service;
 
+import com.backend.domain.chatrequest.dto.response.ChatRequestDto;
+import com.backend.domain.chatrequest.entity.ChatRequest;
+import com.backend.domain.chatrequest.entity.ChatRequestStatus;
+import com.backend.domain.chatrequest.repository.ChatRequestRepository;
+import com.backend.domain.chatroom.entity.ChatRoom;
+import com.backend.domain.chatroom.repository.ChatRoomRepository;
+import com.backend.domain.member.entity.Member;
+import com.backend.domain.member.repository.MemberRepository;
+import com.backend.domain.notification.entity.NotificationType;
+import com.backend.domain.notification.service.NotificationService;
+import com.backend.global.auth.model.CustomUserDetails;
+import com.backend.global.exception.GlobalErrorCode;
+import com.backend.global.exception.GlobalException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class ChatRequestService {
 
-//    private final ChatRequestRepository chatRequestRepository;
-//    private final MemberRepository memberRepository;
-//    private final ChatRoomRepository chatRoomRepository;
-//
-//    @Transactional
-//    public void sendChatRequest(CustomUserDetails loginUser, Long receiverId) {
-//
-//        Member sender = loginUser.getMember();
-//        Member receiver = memberRepository.findById(receiverId).orElseThrow(
-//                () -> new GlobalException(GlobalErrorCode.NOT_FOUND_MEMBER)
-//        );
-//
-//        validateDuplicateRequest(sender, receiver);
-//
-//        ChatRequest request = ChatRequest.builder()
-//                .sender(sender)
-//                .receiver(receiver)
-//                .status(ChatRequestStatus.PENDING)
-//                .requestedAt(LocalDateTime.now())
-//                .build();
-//
-//        chatRequestRepository.save(request);
-//    }
-//
-//    @Transactional
-//    public void respondToRequest(Long requestId, boolean accept) {
-//        ChatRequest request = chatRequestRepository.findById(requestId).orElseThrow(
-//                () -> new GlobalException(GlobalErrorCode.NOT_FOUND_CHAT_REQUEST)
-//        );
-//
-//        if (!request.isPending()) {
-//            throw new GlobalException(GlobalErrorCode.ALREADY_PROCESSED_CHAT_REQUEST);
-//        }
-//
-//        if (accept) {
-//            request.accept();
-//            createChatRoom(request);
-//        } else {
-//            request.reject();
-//        }
-//    }
-//
-//
-//    private void createChatRoom(ChatRequest request) {
-//        ChatRoom chatRoom = ChatRoom.builder()
-//                .sender(request.getSender())
-//                .receiver(request.getSender())
-//                .createdAt(LocalDateTime.now())
-//                .build();
-//
-//        chatRoomRepository.save(chatRoom);
-//    }
-//
-//    private void validateDuplicateRequest(Member sender, Member receiver) {
-//        if (chatRequestRepository.existsBySenderAndReceiver(sender, receiver)) {
-//            throw new GlobalException(GlobalErrorCode.ALREADY_REQUESTED);
-//        }
-//    }
-//
-//    public List<ChatRequestDto> getSentRequests(CustomUserDetails loginUser) {
-//        Member sender = loginUser.getMember();
-//
-//        return chatRequestRepository.findAllBySender(sender).stream()
-//                .map(ChatRequestDto :: of)
-//                .collect(Collectors.toList());
-//    }
-//
-//    public List<ChatRequestDto> getReceivedRequests(CustomUserDetails loginUser) {
-//        Member receiver = loginUser.getMember();
-//
-//        return chatRequestRepository.findAllByReceiver(receiver).stream()
-//                .map(ChatRequestDto :: of)
-//                .collect(Collectors.toList());
-//    }
+    private final ChatRequestRepository chatRequestRepository;
+    private final MemberRepository memberRepository;
+    private final ChatRoomRepository chatRoomRepository;
+    private final NotificationService notificationService;
+
+    @Transactional
+    public void sendChatRequest(CustomUserDetails loginUser, Long receiverId) {
+
+        Member sender = memberRepository.findById(loginUser.getMemberId()).orElseThrow(
+                () -> new GlobalException(GlobalErrorCode.MEMBER_NOT_FOUND)
+        );
+        Member receiver = memberRepository.findById(receiverId).orElseThrow(
+                () -> new GlobalException(GlobalErrorCode.MEMBER_NOT_FOUND)
+        );
+
+        validateDuplicateRequest(sender, receiver);
+
+        ChatRequest request = ChatRequest.builder()
+                .sender(sender)
+                .receiver(receiver)
+                .status(ChatRequestStatus.PENDING)
+                .requestedAt(LocalDateTime.now())
+                .build();
+
+        // 알림설정
+        notificationService.sendNotification(receiverId, NotificationType.REQUEST, loginUser.getMemberId());
+
+        chatRequestRepository.save(request);
+    }
+
+    @Transactional
+    public void respondToRequest(Long requestId, boolean accept) {
+        ChatRequest request = chatRequestRepository.findById(requestId).orElseThrow(
+                () -> new GlobalException(GlobalErrorCode.NOT_FOUND_CHAT_REQUEST)
+        );
+
+        if (!request.isPending()) {
+            throw new GlobalException(GlobalErrorCode.ALREADY_PROCESSED_CHAT_REQUEST);
+        }
+
+        if (accept) {
+            request.accept();
+            createChatRoom(request);
+        } else {
+            request.reject();
+        }
+    }
+
+
+    private void createChatRoom(ChatRequest request) {
+        ChatRoom chatRoom = ChatRoom.builder()
+                .sender(request.getSender())
+                .receiver(request.getSender())
+                .createAt(LocalDateTime.now())
+                .build();
+
+        chatRoomRepository.save(chatRoom);
+    }
+
+    private void validateDuplicateRequest(Member sender, Member receiver) {
+        if (chatRequestRepository.existsBySenderAndReceiver(sender, receiver)) {
+            throw new GlobalException(GlobalErrorCode.ALREADY_REQUESTED);
+        }
+    }
+
+    public List<ChatRequestDto> getSentRequests(CustomUserDetails loginUser) {
+        Member sender = memberRepository.findById(loginUser.getMemberId()).orElseThrow(
+                () -> new GlobalException(GlobalErrorCode.MEMBER_NOT_FOUND)
+        );
+
+        return chatRequestRepository.findAllBySender(sender).stream()
+                .map(ChatRequestDto :: of)
+                .collect(Collectors.toList());
+    }
+
+    public List<ChatRequestDto> getReceivedRequests(CustomUserDetails loginUser) {
+
+        Member receiver = memberRepository.findById(loginUser.getMemberId()).orElseThrow(
+                () -> new GlobalException(GlobalErrorCode.MEMBER_NOT_FOUND)
+        );
+
+        return chatRequestRepository.findAllByReceiver(receiver).stream()
+                .map(ChatRequestDto:: of)
+                .collect(Collectors.toList());
+    }
 
 
 }
