9995e5102460ea6522917683d054996502b0511d
richard3251 <paladog31@naver.com>
2025-04-08 11:37:14 +0900

    feat: keyword패키지 및 키워드 카테고리, 키워드 리스트 조회 기능 완료.

--- backend/src/main/java/com/backend/domain/keyword/repository/KeywordRepository.java
+++ backend/src/main/java/com/backend/domain/keyword/repository/KeywordRepository.java
@@ -3,11 +3,11 @@ package com.backend.domain.keyword.repository;
 import com.backend.domain.keyword.entity.Keyword;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Collection;
+import java.util.List;
 
 public interface KeywordRepository extends JpaRepository<Keyword, Long> {
 
-    Collection<Keyword> findAllByCategoryId(Long categoryId);
+    List<Keyword> findAllByCategoryId(Long categoryId);
 }
 
 
