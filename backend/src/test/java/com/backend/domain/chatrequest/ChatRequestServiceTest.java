59850c62c9066908a69e57f21e5ba50716b45c2a
richard3251 <paladog31@naver.com>
2025-04-10 10:02:56 +0900

    test: chatRequest test start

---
+++ backend/src/test/java/com/backend/domain/chatrequest/ChatRequestServiceTest.java
@@ -0,0 +1,58 @@
+package com.backend.domain.chatrequest;
+
+import com.backend.domain.chatrequest.repository.ChatRequestRepository;
+import com.backend.domain.chatrequest.service.ChatRequestService;
+import com.backend.domain.chatroom.repository.ChatRoomRepository;
+import com.backend.domain.member.entity.Member;
+import com.backend.domain.member.repository.MemberRepository;
+import com.backend.domain.notification.service.NotificationService;
+import com.backend.global.auth.model.CustomUserDetails;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+public class ChatRequestServiceTest {
+
+    private ChatRequestRepository chatRequestRepository;
+    private MemberRepository memberRepository;
+    private ChatRoomRepository chatRoomRepository;
+    private NotificationService notificationService;
+
+    private ChatRequestService chatRequestService;
+
+    private Member sender;
+    private Member receiver;
+    private CustomUserDetails loginUser;
+
+    @BeforeEach
+    void setUp() {
+
+        // 모든 의존성을 Mock 객체로 초기화
+        chatRequestRepository = mock(ChatRequestRepository.class);
+        memberRepository = mock(MemberRepository.class);
+        chatRoomRepository = mock(ChatRoomRepository.class);
+        notificationService = mock(NotificationService.class);
+
+        chatRequestService = new ChatRequestService(
+                chatRequestRepository,
+                memberRepository,
+                chatRoomRepository,
+                notificationService
+        );
+
+        sender = Member.builder().id(1L).build();
+        receiver = Member.builder().id(2L).build();
+
+        loginUser = new CustomUserDetails(sender.getId(), sender.getEmail(), List.of());
+    }
+
+
+
+
+
+
+
+
+}
