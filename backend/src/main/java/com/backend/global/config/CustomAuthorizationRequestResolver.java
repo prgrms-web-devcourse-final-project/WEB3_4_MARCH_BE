4ff2a753f4cac4509be16a578fc11069722ab799
wlsdnjs1120 <wlsdnjs1120@naver.com>
2025-04-15 14:13:10 +0900

    fix: CustomAuthorizationRequestResolver 오류 수정

--- backend/src/main/java/com/backend/global/config/CustomAuthorizationRequestResolver.java
+++ backend/src/main/java/com/backend/global/config/CustomAuthorizationRequestResolver.java
@@ -9,9 +9,11 @@ import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 @Component
 public class CustomAuthorizationRequestResolver implements OAuth2AuthorizationRequestResolver {
+
     private final DefaultOAuth2AuthorizationRequestResolver defaultResolver;
 
     public CustomAuthorizationRequestResolver(ClientRegistrationRepository clientRegistrationRepository) {
@@ -39,12 +41,12 @@ public class CustomAuthorizationRequestResolver implements OAuth2AuthorizationRe
 
         Map<String, Object> additionalParameters = new HashMap<>(authorizationRequest.getAdditionalParameters());
         if (redirectUrl != null && !redirectUrl.isEmpty()) {
-            additionalParameters.put("state", redirectUrl);
+            additionalParameters.put("redirectUrl", redirectUrl);
         }
 
         return OAuth2AuthorizationRequest.from(authorizationRequest)
                 .additionalParameters(additionalParameters)
-                .state(redirectUrl)
+                .state(UUID.randomUUID().toString())
                 .build();
     }
 }
