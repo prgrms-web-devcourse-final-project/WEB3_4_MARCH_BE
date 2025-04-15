1fef2d7bd48e193a3417b987d5efd76e57480f04
wlsdnjs1120 <wlsdnjs1120@naver.com>
2025-04-15 22:09:09 +0900

    feat: 에러 로그

--- backend/src/main/java/com/backend/global/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/backend/global/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.backend.global.exception;
 import com.backend.global.response.GenericResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -17,6 +18,7 @@ import java.util.List;
  * GlobalExceptionHandler
  * 전역 예외를 처리하기 위한 클래스
  */
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -25,6 +27,9 @@ public class GlobalExceptionHandler {
      */
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<GenericResponse<?>> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpServletRequest request) {
+
+        log.info(" @Valid 유효성 검사 실패 시 발생하는 예외 처리",ex);
+
         List<String> errorMessages = new ArrayList<>();
 
         for (FieldError error : ex.getBindingResult().getFieldErrors()) {
@@ -43,6 +48,7 @@ public class GlobalExceptionHandler {
     public ResponseEntity<GenericResponse<?>> handleConstraintViolationException(ConstraintViolationException ex, HttpServletRequest request) {
         List<String> errorMessages = new ArrayList<>();
         ex.getConstraintViolations().forEach(v -> errorMessages.add(v.getPropertyPath() + ": " + v.getMessage()));
+        log.info(" @PathVariable 등의 유효성 검사 실패 시 발생하는 예외 처리",ex);
 
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
@@ -60,6 +66,8 @@ public class GlobalExceptionHandler {
 //    }
     @ExceptionHandler(GlobalException.class)
     public ResponseEntity<GenericResponse<?>> handleGlobalException(GlobalException ex) {
+        log.info("Global 예외 처리",ex);
+
         GlobalErrorCode errorCode = ex.getGlobalErrorCode();
         return ResponseEntity
                 .status(errorCode.getHttpStatus())
@@ -82,6 +90,7 @@ public class GlobalExceptionHandler {
      */
     @ExceptionHandler(Exception.class)
     public ResponseEntity<GenericResponse<?>> handleAllUnhandledException(Exception ex, HttpServletRequest request) {
+        log.info("예외가 명시되지 않은 모든 예외 처리",ex);
         return ResponseEntity
             .status(HttpStatus.INTERNAL_SERVER_ERROR)
             .body(GenericResponse.fail(HttpStatus.INTERNAL_SERVER_ERROR.value(), "알 수 없는 서버 오류가 발생했습니다."));
